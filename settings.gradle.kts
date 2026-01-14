40cf19b672b3069c28c8decb7f81841437f78796
DovydasKelecius <Kelecius.Dovydas@gmail.com>
2026-01-15 00:01:48 +0200

    hotfix agane

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,2 @@
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id 'org.gradle.toolchains.foojay-resolver-convention' version '1.0.0';
-}
-
 rootProject.name = 'TemplatePlugin'
 include('app')
